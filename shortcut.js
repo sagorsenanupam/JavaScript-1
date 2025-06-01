7b32471f20659a267aeec748428120f4b9d49ebc
sagorsenanupam <senanupam10@gmail.com>
2025-06-01 23:32:36 +0600

    edited

---
+++ shortcut.js
@@ -0,0 +1,6 @@
+const isLeader = true;
+if (isLeader) {
+  console.log("Leader ashche chair charo");
+} else {
+  console.log("Vitore dukte dibo na");
+}
--- ternary.js
+++ ternary.js
@@ -39,4 +39,5 @@ const isLeader = true;
 // }
 
 // Ternary Method
-price = isLeader === True ? (price > 1000 ? price / 2 : 0) : price + 1000;
+price = isLeader === true ? (price > 1000 ? price / 2 : 0) : price + 1000;
+console.log(price);
